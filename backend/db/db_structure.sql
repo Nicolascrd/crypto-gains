c4ae369c52670be9a145a4a76a38ee0de9a0d297
Nicolas Couraud <sp.couraud.nicolas@gmail.com>
2023-01-20 11:39:25 +0100

    new table deposits and withdrawals

--- backend/db/db_structure.sql
+++ backend/db/db_structure.sql
@@ -2,3 +2,10 @@ CREATE TABLE keys (key_id INTEGER PRIMARY KEY,
 name TEXT NOT NULL,
 public_key TEXT NOT NULL,
 secret_key TEXT NOT NULL);
+
+CREATE TABLE depositsWithdrawals(dw_id INTEGER PRIMARY KEY, 
+account INTEGER NOT NULL, 
+utc_time INTEGER, 
+asset TEXT NOT NULL, 
+change REAL NOT NULL, 
+FOREIGN KEY(account) REFERENCES keys(key_id));
