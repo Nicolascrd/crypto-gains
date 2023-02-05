f028d3b7f189fee484abe1383977ba9c762600f2
Nicolas Couraud <sp.couraud.nicolas@gmail.com>
2023-02-05 12:55:36 +0100

    eslint, trading pairs binance and kraken

--- backend/controllers/binanceController.ts
+++ backend/controllers/binanceController.ts
@@ -3,12 +3,7 @@ import { publicAndSecretKey } from "./dbController.js";
 import { GetAccount } from "./interfaces.js";
 
 async function clientFromId(id: number) {
-  let public_key: string, secret_key: string;
-  try {
-    ({ public_key, secret_key } = await publicAndSecretKey(id));
-  } catch (e) {
-    throw e;
-  }
+  const { public_key, secret_key } = await publicAndSecretKey(id);
   return new MainClient({
     api_key: public_key,
     api_secret: secret_key,
@@ -16,26 +11,21 @@ async function clientFromId(id: number) {
 }
 
 export const getAccount = async (id: number) => {
-  let client;
-  try {
-    client = await clientFromId(id);
-  } catch (err) {
-    throw err;
-  }
-  let res: GetAccount = {
+  const client = await clientFromId(id);
+  const res: GetAccount = {
     amounts: {} as Record<string, number>,
     tickers: [] as string[],
   };
   return await client.getAccountInformation().then((result) => {
     console.log(result.balances);
     result.balances.forEach((el) => {
-      let fr = typeof el.free == "number" ? el.free : parseFloat(el.free);
-      let lock =
+      const fr = typeof el.free == "number" ? el.free : parseFloat(el.free);
+      const lock =
         typeof el.locked == "number" ? el.locked : parseFloat(el.locked);
       if (fr > 0 || lock > 0) {
         if (el.asset.substring(0, 2) == "LD" && el.asset.length > 3) {
           // lended asset counted as asset
-          let ass = el.asset.substring(2);
+          const ass = el.asset.substring(2);
           if (ass in res.amounts) {
             res.amounts[ass] += fr + lock;
           } else {
@@ -60,7 +50,7 @@ export const getPrices = async (tickers: string[]) => {
   const client = new MainClient({});
   const promises = [];
 
-  for (let t of tickers) {
+  for (const t of tickers) {
     promises.push(client.getAvgPrice({ symbol: t + "USDT" }));
   }
   return Promise.all(promises)
