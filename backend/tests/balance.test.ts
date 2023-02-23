7a0ab11bec238688979767e1699118d883b3b42e
Nicolas Couraud <sp.couraud.nicolas@gmail.com>
2023-02-23 15:33:45 +0100

    change scripts to protect db if test fails

--- backend/tests/balance.test.ts
+++ backend/tests/balance.test.ts
@@ -2,14 +2,16 @@ import request from "supertest";
 import app from "./../src/app";
 import { assert } from "chai";
 
+const idsToTest = [1, 3]
+
 describe("POST /balance", () => {
   it("should return code 200", async () => {
-    await request(app).post("/balance").send([3]).expect(200);
+    await request(app).post("/balance").send(idsToTest).expect(200);
   });
   it("should return an object balance", async () => {
     await request(app)
       .post("/balance")
-      .send([3])
+      .send(idsToTest)
       .set("Accept", "application/json")
       .then((response) => {
         const bodyResponse = response.body;
